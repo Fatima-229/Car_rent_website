674681263f19bfcb6ce2f33896e80fd65ad7c6f2
Fatima-229 <159787658+Fatima-229@users.noreply.github.com>
2025-02-07 17:17:21 +0500

    updated

---
+++ types/products.ts
@@ -0,0 +1,18 @@
+export interface Cars {
+    id: string; 
+    name: string; 
+    brand: string; 
+    type: string; 
+    fuelCapacity: string;
+    transmission: string; 
+    seatingCapacity: string; 
+    pricePerDay: number; 
+    originalPrice?: number; 
+    tags?: string[]; 
+    image: {
+      asset: {
+        _ref: string; 
+        _type: "reference";
+      };
+    };
+}
